5ec8d5cd216aaf4d7297fdaabb0e06ea0be190a8
alaincosgaya <alsucar02@gmail.com>
2021-11-12 00:40:10 +0100

    Implementacion del dao final

--- src/logic/DaoImplementation.java
+++ src/logic/DaoImplementation.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import classes.UserInfo;
 import classes.User;
 import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * La implementacion del DAO con la cual se ejecutan las acciones solicitadas
@@ -15,12 +17,19 @@ import java.sql.Connection;
  *
  * @author Alain Cosgaya y Alejandro Gomez
  */
-public class DaoImplementation implements Signable{
-    
+public class DaoImplementation implements Signable {
+
+    private static final Logger LOGGER = Logger.getLogger(DaoImplementation.class.getName());
+    private final Pool pool;
     private Connection con = null;
     private PreparedStatement stmt = null;
     private final String signInUser = "CALL `login_validator`(?,?)";
     private final String signUpUser = "CALL `register_validator`(?,?,?,?)";
+    
+
+    public DaoImplementation() throws ConnectException {
+        pool = Pool.getInstance();
+    }
 
     /**
      * Metodo que comprueba a traves de un procedimiento almacenado si los
@@ -31,8 +40,9 @@ public class DaoImplementation implements Signable{
      *
      * @param message Los datos introducidos por el usuario por validar.
      *
-     * @return Objeto tipo UserInfo con los datos del usuario y un mensaje de
-     * tipo enum con lo ocurrido a lo largo de la ejecucion del metodo.
+     * @return Objeto tipo User con los datos del usuario devueltos a la hora de
+     * hacer la ejecucion del metodo.
+     *
      *
      * @throws ConnectException
      *
@@ -41,44 +51,47 @@ public class DaoImplementation implements Signable{
      * @throws UpdateException
      */
     @Override
-    public User signIn(User message) throws ConnectException, SignInException, UpdateException {
+    public synchronized User signIn(User message) throws ConnectException, SignInException, UpdateException {
+
         User user;
         ResultSet rs = null;
-        //try {
-            con = Pool.getInstance().getConnection();
-        /*} catch (SQLException e) {
-            throw new ConnectException("Error al intentar conectarse al servidor, intentelo mas tarde");
-        }*/
+        con = pool.getConnection();
         try {
-
+            LOGGER.info("Prepara procedimiento de inicio de sesion");
             stmt = con.prepareStatement(signInUser);
             stmt.setString(1, message.getUsername());
             stmt.setString(2, message.getPassword());
+            LOGGER.info("Ejecuta procedimiento");
             rs = stmt.executeQuery();
             if (rs.next()) {
+                LOGGER.info("Recoge los parametros devueltos por el procedimiento");
                 user = new User();
                 user.setId(rs.getLong("users.id"));
                 user.setUsername(rs.getString("users.login"));
                 user.setEmail(rs.getString("users.email"));
                 user.setFullName(rs.getString("users.fullName"));
                 user.setPassword(rs.getString("users.password"));
-                
+
             } else {
-                throw new SignInException("Los parametros introducidos no corresponden a ningún cliente");
+                throw new SignInException("Los parametros introducidos no corresponden a ningun cliente");
             }
         } catch (SQLException e) {
+            Logger.getLogger(DaoImplementation.class.getName()).log(Level.SEVERE, null, e);
             throw new UpdateException("Error al intentar verificar los parametros en la base de datos");
 
-        }
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-                throw new ConnectException("Error al intentar cerrar la conexion al servidor, intentelo mas tarde");
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    Logger.getLogger(DaoImplementation.class.getName()).log(Level.SEVERE, null, e);
+                    throw new ConnectException("Error al intentar cerrar la conexion al servidor, intentelo mas tarde");
+                }
             }
-            
-
+            LOGGER.info("Se libera la conexion del pool");
+            pool.freeConnection(con);
         }
+
         return user;
     }
 
@@ -92,48 +105,48 @@ public class DaoImplementation implements Signable{
      * @param message Los datos introducidos por el usuario para su validacion y
      * registro.
      *
-     * @return Objeto tipo UserInfo con los datos del usuario y un mensaje de
-     * tipo enum con lo ocurrido a lo largo de la ejecucion del metodo.
-     * 
+     * @return Objeto tipo User con los datos del usuario devueltos a la hora de
+     * hacer la ejecucion del metodo.
+     *
      * @throws ConnectException
-     * 
+     *
      * @throws SignUpException
      *
      * @throws UpdateException
      */
     @Override
-    public User signUp(User message) throws ConnectException, SignUpException, UpdateException {
-        boolean existe;
-        ResultSet rs = null;
-       /* try {
-            
-        } catch (SQLException e) {
-            throw new ConnectException("Error al intentar conectarse al servidor, intentelo mas tarde");
-        }*/
-       con = Pool.getInstance().getConnection();
-        try {
+    public synchronized User signUp(User message) throws ConnectException, SignUpException, UpdateException {
 
+        ResultSet rs = null;
+        con = pool.getConnection();
+        try {
+            LOGGER.info("Prepara procedimiento de registro");
             stmt = con.prepareStatement(signUpUser);
             stmt.setString(1, message.getUsername());
             stmt.setString(2, message.getEmail());
             stmt.setString(3, message.getFullName());
             stmt.setString(4, message.getPassword());
+            LOGGER.info("Ejecuta procedimiento");
             rs = stmt.executeQuery();
-            existe = rs.getBoolean("existe");
-            if (existe) {
-                throw new SignUpException("El nombre de usuario o email corresponden a otro cliente");
-              
+            LOGGER.info("Comprueba si se ha devuelto algun parametro");
+            if (rs.next()) {
+                throw new SignUpException("Los parametros introducidos corresponden a un cliente ya existente.");
             }
 
         } catch (SQLException e) {
+            Logger.getLogger(DaoImplementation.class.getName()).log(Level.SEVERE, null, e);
             throw new UpdateException("Error al intentar registrar la conexión en la base de datos");
-        }
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-                throw new ConnectException("Error al intentar cerrar la conexion al servidor, intentelo mas tarde");
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    Logger.getLogger(DaoImplementation.class.getName()).log(Level.SEVERE, null, e);
+                    throw new ConnectException("Error al intentar cerrar la conexion al servidor, intentelo mas tarde");
+                }
             }
+            LOGGER.info("Se libera la conexion del pool");
+            pool.freeConnection(con);
         }
         return message;
 
