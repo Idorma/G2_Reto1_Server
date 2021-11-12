5362052483fbd29874a91c106fa4a2aee77057cd
TheAlex557 <83644488+TheAlex557@users.noreply.github.com>
2021-11-12 09:07:03 +0100

    Main con serverSocket, hilo de cierre e hilo de entrada

--- src/application/App.java
+++ src/application/App.java
@@ -1,69 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * App será la clase que contenga todo el código relacionado con el conexionado
+ * del cliente con el servidor.
  */
 package application;
 
-import classes.User;
 import exceptions.ConnectException;
 import exceptions.SignInException;
 import exceptions.UpdateException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import logic.DaoFactory;
-import logic.DaoImplementation;
+import logic.HiloCierre;
 import logic.HiloEntrada;
 
 /**
+ * La clase App será la encargada de gestionar el número de usuarios que pueden
+ * conectarse
  *
- * @author 2dam
+ * @author Jonathan Camacho y Alejandro Gomez
  */
 public class App {
 
-    /**
-     * @param args the command line arguments
-     */
-    private final static int port = 5001;
-    
-    public static void main(String[] args) throws ConnectException, SignInException, UpdateException {
-        // TODO code application logic here
-        
-     /*   DaoFactory facto = new DaoFactory();
-        User user = new User();
-        user.setUsername("alain");
-        user.setPassword("abcd*1234");
-        
-            facto.getDao().signIn(user);
-        
-        */
+    private final static Logger LOGGER = Logger.getLogger(App.class.getName());
+    private final static ResourceBundle configFile = ResourceBundle.getBundle("archives.config");
+    private static int port;
+    private static int max_connections;
 
+    private static int contador = 0;
+
+    /**
+     * Método que se encargará de gestionar el acceso de todos aquellos clientes
+     * que se conecten al servidor mediante sockets e hilos.
+     *
+     * @param args
+     * @throws ConnectException
+     * @throws SignInException
+     * @throws UpdateException
+     */
+    public static void main(String[] args) throws ConnectException, SignInException, UpdateException {
+        port = Integer.valueOf(configFile.getString("port"));
+        max_connections = Integer.valueOf(configFile.getString("maxServerConnections"));
+        boolean max;
         ServerSocket servidor = null;
-        
+
         try {
+            max=false;
             servidor = new ServerSocket(port);
-            System.out.println("Servidor iniciado");
-            
+            LOGGER.info("Lado servidor iniciado");
+
             Socket clienteSocket = null;
-            
-            while(true){
-                clienteSocket=servidor.accept();
-                System.out.println("Cliente conectado");
-                
-                
-                
-                HiloEntrada hilo = new HiloEntrada(clienteSocket);
-                hilo.start();
+            HiloCierre hiloCierre = new HiloCierre();
+            hiloCierre.start();
+            while (true) {
+                clienteSocket = servidor.accept();
+                LOGGER.info("Conexion con cliente iniciada");
+                contador++;
+                LOGGER.info("Comprobacion de si se ha llegado al limite de conexiones simultaneas");
+                if (contador <= max_connections) {
+                    max = false;
+                    LOGGER.info("Creacion de hilo para el control de la peticion del cliente");
+                    
+
+                } else {
+                    max=true;
+                    LOGGER.warning("El servidor esta lleno");
+                   
+
+                }
+                HiloEntrada hilo = new HiloEntrada(clienteSocket,max);
+                    hilo.start();
+
             }
-            
-            
         } catch (IOException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
+    /**
+     * Método el cual permite al server controlar el número de hilos en uso.
+     *
+     */
+    public static void desconexion() {
+        contador--;
+    }
+
 }
